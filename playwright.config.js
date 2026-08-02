f905783bb9db90222d3df84830543da870a0c2ff
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2026-08-02 12:30:42 -0400

    Playwright Automation

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: 4,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -28,12 +28,19 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-   
+    trace: "on-first-retry",
+   // screenshot: "on-first-failure",
+   // video: {mode: "on-first-retry", size: {width: 1900, height: 1080}},
+
+    launchOptions: {
+      slowMo: 0, // slow down by given milliseconds
+    },
+
   },
 
   /* Configure projects for major browsers */
   projects: [
+    
     {
       name: 'chromium',
       use: { 
@@ -43,6 +50,14 @@ module.exports = defineConfig({
     },
 
     /*
+    {
+      name: 'chromium',
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport: {width: 1900, height: 1080} 
+      },
+    },
+
     {
       name: 'firefox',
       use: { 
@@ -58,34 +73,41 @@ module.exports = defineConfig({
         viewport: {width: 1850, height: 1000}  
       },
     },
+
+     {
+      name: 'Google Chrome',
+      use: { 
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+        viewport: {width: 1900, height: 1080} 
+      },
+    },
+
+    {
+      name: 'Microsoft Edge',
+      use: { 
+        ...devices['Desktop Edge'],
+        channel: 'msedge',
+        viewport: {width: 1900, height: 1080} 
+      },
+    },
+
+     {
+      name: 'Mobile Chrome',
+      use: { 
+        ...devices['Galaxy Note 3'],
+      },
+    },
+
+    {
+      name: 'Mobile Safari',
+      use: { 
+        ...devices['iPhone 13'],
+      },
+    },
 */
-   
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+ 
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
 
